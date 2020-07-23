1f6a2dd6ca5b1a08dae73c9801000be67e5ad9f0
Alan Flood <alanwflood.tech@gmail.com>
2020-07-24 00:41:17 +0100

    Finishing up Search

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,58 @@
-// import { readFileStr } from "https://deno.land/std/fs/mod.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts";
+
 // import { red, green, blue, bold } from "https://deno.land/std/fmt/colors.ts";
 
 class TrieNode {
-  key: string | undefined;
-  parent: TrieNode | undefined;
+  word: string | undefined;
   children: Map<string, TrieNode>;
-  isEndOfWord: Boolean;
 
-  constructor(key: string, parent: TrieNode, isEndOfWord = false) {
-    this.key = key;
-    this.parent = parent;
+  constructor(word?: string) {
+    this.word = word;
     this.children = new Map();
-    this.isEndOfWord = isEndOfWord;
   }
 }
 
 interface TrieInterface {
-  editCount: number;
   insert(word: string): void;
   contains(word: string): boolean;
-  similar(word: string): string;
+  search(word: string, maxCost: number): string;
 }
 
 class Trie implements TrieInterface {
-  private rootNode: TrieNode = {
-    key: undefined,
-    parent: undefined,
-    children: new Map(),
-    isEndOfWord: false
-  };
+  private rootNode: TrieNode = new TrieNode();
 
-  insert(word: string) {
+  private insertWord(word: string): void {
     let currentNode: TrieNode = this.rootNode;
 
-    word.split("").forEach((letter: string, index) => {
-      let nextNode: TrieNode | undefined = currentNode.children.get(letter);
+    word.split("").forEach((letter) => {
+      let child = currentNode.children.get(letter);
 
-      const isLastLetter = index === word.length - 1;
-      if (isLastLetter || nextNode === undefined) {
-        if (nextNode === undefined) nextNode = new TrieNode(letter, currentNode);
-        if (isLastLetter) nextNode.isEndOfWord = true;
-        currentNode.children.set(letter, nextNode);
+      if (child === undefined) {
+        child = new TrieNode(word);
+        currentNode.children.set(letter, child);
       }
 
-      currentNode = nextNode;
+      currentNode = child;
     });
+
+    if (currentNode.word !== word) currentNode.word = word;
+  }
+
+  insert(words: string | string[]): void {
+    Array.isArray(words)
+      ? words.forEach((word) => this.insertWord(word))
+      : this.insertWord(words);
   }
 
   contains(word: string) {
     let currentNode: TrieNode = this.rootNode;
 
-    word.split("").forEach((letter: string) => {
-      const nextNode: TrieNode | undefined = currentNode.children.get(letter);
-
-      if (nextNode !== undefined) {
-        currentNode = nextNode;
-      } else {
-        return false;
-      }
-    });
+    for (let i = 0; i < word.length; i++) {
+      let letter = word.charAt(i);
+      const nextNode = currentNode.children.get(letter);
+      if (nextNode !== undefined) currentNode = nextNode;
+      else return false;
+    }
     return true;
   }
 
@@ -67,7 +61,7 @@ class Trie implements TrieInterface {
     letter: string,
     word: string,
     previousRow: ReadonlyArray<number>,
-    result: string[],
+    results: string[],
     maxCost: number
   ) {
     const previousColumn = word.length;
@@ -75,21 +69,26 @@ class Trie implements TrieInterface {
 
     for (let column = 1; column <= previousColumn; column++) {
       let replaceCost: number;
-      let insertCost = currentRow[column - 1] + 1;
-      let deleteCost = previousRow[column] + 1;
+      const insertCost = currentRow[column - 1] + 1;
+      const deleteCost = previousRow[column] + 1;
+
       if (word[column - 1] !== letter) {
-        replaceCost = previousRow[ column - 1 ] + 1;
+        replaceCost = previousRow[column - 1] + 1;
       } else {
-        replaceCost = previousRow[ column - 1 ];
+        replaceCost = previousRow[column - 1];
       }
 
-      currentRow[column] = Math.min(
-        deleteCost,
-        insertCost,
-        replaceCost
-      );
+      currentRow[column] = Math.min(deleteCost, insertCost, replaceCost);
+    }
 
-    return currentRow[-1]
+    if (currentRow[previousColumn] <= maxCost && node.word !== undefined) {
+      results.push(node.word);
+    }
+
+    if (Math.min(...currentRow) <= maxCost) {
+      node.children.forEach((node, letter) => {
+        this.searchRecursive(node, letter, word, currentRow, results, maxCost);
+      });
     }
   }
 
@@ -97,23 +96,26 @@ class Trie implements TrieInterface {
     const currentRow = [...Array(word.length + 1).keys()];
     const results: string[] = [];
 
-    this.rootNode.children.forEach((trieNode: TrieNode, letter: String) => {
-      this.searchRecursive(trieNode, letter, word, currentRow, results, maxCost)
-    })
-    
-    let currentNode: TrieNode = this.rootNode;
-
-    word.split("").forEach((letter: string) => {
-      const nextNode: TrieNode | undefined = currentNode.children.get(letter);
-
-      if (nextNode !== undefined) {
-        currentNode = nextNode;
-      } else {
-        ;
-      }
+    this.rootNode.children.forEach((node, letter) => {
+      this.searchRecursive(node, letter, word, currentRow, results, maxCost);
     });
-    return true;
-  }
 
+    return results.length
+      ? `Looking for: ${word}\n Found: ${results[0]}`
+      : `Could not find word similar to ${word}`;
   }
 }
+let args = parse(Deno.args);
+console.dir(args);
+let filename = String(args._[0]);
+
+const decoder = new TextDecoder();
+let text = decoder.decode(Deno.readFileSync(filename));
+let words = text
+  .replace(/\n/, " ")
+  .split(" ")
+  .map((str) => str.replace(/[^0-9a-z]/gi, "").toLowerCase());
+
+const trie = new Trie();
+trie.insert(words);
+console.log(trie.contains("myrble"));
